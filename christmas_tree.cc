f5c8f37575dfbc092936c8b051486f7d7a320850
Ryan Vo <ryanv7443@csu.fullerton.edu>
2025-12-10 10:57:26 -0800

    need help

--- christmas_tree.cc
+++ christmas_tree.cc
@@ -1,5 +1,22 @@
 #include <iostream>
 int main() {
-    std::cout << "Enter the height of the Christmas tree: "
-    std::cin >> height
+    int height;
+    std::cout << "Enter the height of the Christmas tree: ";
+    std::cin >> height;
+    for (int i = 1; i<= height; i++ ){
+        for (int s = 0; s < height - i; s++) {
+        }
+    
+    for (int star = 0; star < ( 2 * i -1); star ++){
+        std::cout << "*";
+    }
+}
+std::cout << "\n";
+for (int t = 0; t < 3; t++) {
+    for (int s = 0; s < height - 1; s++) {
+        std::cout << " ";
+    }
+    std::cout << "#\n";
+}
+return 0 ;
 }
